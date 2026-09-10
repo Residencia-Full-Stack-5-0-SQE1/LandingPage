330c7c314556363937a11d57125fac8c7ba2c7cb
KayqueLeoterio <kayqueleoterio.ss014@academico.ifsul.edu.br>
2026-09-10 15:00:17 -0300

    pop-up feitos

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ document.querySelectorAll('header a[href^="#"]').forEach(link => {
     }
   });
 });
+
+const modal = document.getElementById("modal");
+
+const modalNome = document.getElementById("modal-nome");
+const modalImg = document.getElementById("modal-img");
+const modalDescricao = document.getElementById("modal-descricao");
+const modalLinkedin = document.getElementById("modal-linkedin");
+
+document.querySelectorAll(".integrantes").forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        modalNome.innerHTML =
+            `<span class="tag">&lt;</span>${card.dataset.nome}<span class="tag">&gt;</span>`;
+
+        modalImg.src = card.dataset.foto;
+
+        modalDescricao.textContent = card.dataset.descricao;
+
+        modalLinkedin.href = card.dataset.linkedin;
+
+        modal.style.display = "flex";
+
+    });
+
+});
+
+document.querySelector(".fechar").addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+modal.addEventListener("click", e => {
+
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+
+});
